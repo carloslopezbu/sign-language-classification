2b772f475508ce18ec0761844020323e632e85e4
Carlos David López Hinojosa <backupcarlos18@gmail.com>
2025-11-02 14:33:28 +0100

    multilang

--- performance/scrapping/src/lib.rs
+++ performance/scrapping/src/lib.rs
@@ -4,9 +4,11 @@ mod trie;
 use trie::_Trie;
 
 mod webcrawler;
+use webcrawler::get_the_meat_balls;
 
 #[pymodule]
 fn scrapping(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<_Trie>()?;
+    m.add_function(wrap_pyfunction!(get_the_meat_balls, m)?)?;
     Ok(())
 }
---
+++ webcrawl.py
@@ -0,0 +1,5 @@
+from scrapping import get_the_meat_balls
+
+
+if __name__ == "__main__":
+    get_the_meat_balls("datamining/clean/StS/metadata.es.es.csv", ["it.it"])
